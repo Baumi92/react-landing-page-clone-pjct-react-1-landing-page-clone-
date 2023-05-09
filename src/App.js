cb65b8f2b4c038521b00330bb5cdb8e94fc56664
Manuel Baumgartner <manu.baumi@outlook.com>
2023-05-09 17:14:57 +0200

    Create app scss

--- src/App.js
+++ src/App.js
@@ -4,9 +4,35 @@ export default function App() {
   return (
     <>
       <header>
-        <div className={styles.promoSection}>
-          <span>asdf</span>
-        </div>
+        <nav>
+          <div>Landingfolio</div>
+          <ul>
+            <li>
+              <a> Inspiration</a>
+            </li>
+            <li>
+              <a> Component Libarary</a>
+            </li>
+            <li>
+              <a> Templates</a>
+            </li>
+            <li>
+              <a> Learn</a>
+            </li>
+            <li>
+              <a> Tools</a>
+            </li>
+            <li>
+              <a> Pricing</a>
+            </li>
+            <li>
+              <a> Support</a>
+            </li>
+            <li>
+              <a> Login</a>
+            </li>
+          </ul>
+        </nav>
       </header>
       <main> Main content </main>
       <footer>My Footer</footer>
